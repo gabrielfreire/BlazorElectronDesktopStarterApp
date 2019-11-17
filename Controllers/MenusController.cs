9b0ca5c2117c05963932fe25d863c6d7aa54c8bd
Gabriel Freire <gabrielfreiredev@gmail.com>
2019-11-17 16:58:39 +0000

    first commit

---
+++ Controllers/MenusController.cs
@@ -0,0 +1,52 @@
+﻿using ElectronNET.API;
+using ElectronNET.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorElectronDesktopStarterApp.Controllers
+{
+    public class MenusController
+    {
+        public static void SetMenu()
+        {
+            if (HybridSupport.IsElectronActive)
+            {
+                var menu = new MenuItem[]
+                {
+                    new MenuItem { Label = "File", Submenu = new MenuItem[]
+                            {
+                                new MenuItem { Label = "Quit", Accelerator = "CmdOrCtrl+Q", Role = MenuRole.close }
+                            }
+                    },
+                    new MenuItem { Label = "View", Submenu = new MenuItem[]
+                            {
+                                new MenuItem { Label = "Reload", Accelerator = "CmdOrCtrl+R", Click = () =>
+                                    {
+                                        var mainWindowId = Electron.WindowManager.BrowserWindows.ToList().First().Id;
+                                        Electron.WindowManager.BrowserWindows.ToList().ForEach(browserWindow =>
+                                        {
+                                            if (browserWindow.Id != mainWindowId)
+                                            {
+                                                browserWindow.Close();
+                                            }
+                                            else
+                                            {
+                                                browserWindow.Reload();
+                                            }
+                                        });
+                                    } 
+                                },
+                                new MenuItem { Type = MenuType.separator },
+                                new MenuItem { Label = "Open Developer Tools", Accelerator = "CmdOrCtrl+I", Click = () => Electron.WindowManager.BrowserWindows.First().WebContents.OpenDevTools() }
+                            }
+                    }
+                };
+
+                Electron.Menu.SetApplicationMenu(menu);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Blazor Desktop App Starter using Electron.NET
+
+Just a starter to help when creating Cross-platform desktop apps using Blazor.
+
