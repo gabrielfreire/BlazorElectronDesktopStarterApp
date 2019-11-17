fbb1fdd8168e06522756cafaeaa18296eab63340
Gabriel Freire <gabrielfreiredev@gmail.com>
2019-11-17 23:42:55 +0000

    refactor menu controller

--- Controllers/MenusController.cs
+++ Controllers/MenusController.cs
@@ -17,31 +17,31 @@ namespace BlazorElectronDesktopStarterApp.Controllers
                 var menu = new MenuItem[]
                 {
                     new MenuItem { Label = "File", Submenu = new MenuItem[]
-                            {
-                                new MenuItem { Label = "Quit", Accelerator = "CmdOrCtrl+Q", Role = MenuRole.close }
-                            }
+                        {
+                            new MenuItem { Label = "Quit", Accelerator = "CmdOrCtrl+Q", Role = MenuRole.close }
+                        }
                     },
                     new MenuItem { Label = "View", Submenu = new MenuItem[]
-                            {
-                                new MenuItem { Label = "Reload", Accelerator = "CmdOrCtrl+R", Click = () =>
+                        {
+                            new MenuItem { Label = "Reload", Accelerator = "CmdOrCtrl+R", Click = () =>
+                                {
+                                    var mainWindowId = Electron.WindowManager.BrowserWindows.ToList().First().Id;
+                                    Electron.WindowManager.BrowserWindows.ToList().ForEach(browserWindow =>
                                     {
-                                        var mainWindowId = Electron.WindowManager.BrowserWindows.ToList().First().Id;
-                                        Electron.WindowManager.BrowserWindows.ToList().ForEach(browserWindow =>
+                                        if (browserWindow.Id != mainWindowId)
                                         {
-                                            if (browserWindow.Id != mainWindowId)
-                                            {
-                                                browserWindow.Close();
-                                            }
-                                            else
-                                            {
-                                                browserWindow.Reload();
-                                            }
-                                        });
-                                    } 
-                                },
-                                new MenuItem { Type = MenuType.separator },
-                                new MenuItem { Label = "Open Developer Tools", Accelerator = "CmdOrCtrl+I", Click = () => Electron.WindowManager.BrowserWindows.First().WebContents.OpenDevTools() }
-                            }
+                                            browserWindow.Close();
+                                        }
+                                        else
+                                        {
+                                            browserWindow.Reload();
+                                        }
+                                    });
+                                } 
+                            },
+                            new MenuItem { Type = MenuType.separator },
+                            new MenuItem { Label = "Open Developer Tools", Accelerator = "CmdOrCtrl+I", Click = () => Electron.WindowManager.BrowserWindows.First().WebContents.OpenDevTools() }
+                        }
                     }
                 };
 
